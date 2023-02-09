1aa8562a8a5813cf2d3ec0022c24fecb03e8b140
negiashish1 <negi.ashish012@gmail.com>
2023-02-10 01:57:46 +0530

    v1

---
+++ src/test/java/com/example/nmss_backend/Nmss_backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nmss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
