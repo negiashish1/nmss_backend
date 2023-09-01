efbb1c38e08c801032a18c33710a8a52eeb0825f
negiashish1 <negi.ashish012@gmail.com>
2023-09-02 02:22:13 +0530

    ed

--- src/main/java/com/example/nmss_backend/Nmss_backendApplication.java
+++ src/main/java/com/example/nmss_backend/Nmss_backendApplication.java
@@ -1,4 +1,4 @@
-package com.example.nmss;
+package com.example.nmss_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/nmss_backend/Nmss_backendApplicationTests.java
+++ src/test/java/com/example/nmss_backend/Nmss_backendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.nmss;
+package com.example.nmss_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
